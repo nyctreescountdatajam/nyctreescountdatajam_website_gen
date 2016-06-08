b8aaafbc3cb6b427e15c288a3380b8df4f20742e
Danny S <danielmsheehan@iMac.home>
2016-06-08 18:21:48 -0400

    copy index

--- 02-copy-index.py
+++ 02-copy-index.py
@@ -2,4 +2,4 @@ import shutil
 
 wd = '/Users/danielmsheehan/GitHub/nyctreescountdatajam.github.com/map/'
 
-shutil.copy2(wd+'cttrees1995.html', wd+'index.html')
+shutil.copy2(wd+'cttrees2015.html', wd+'index.html')
